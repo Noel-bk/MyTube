de22d44a7161c17cf5bafe548072072d44301ef6
Noel.b <jaeyeong.b@gmail.com>
2017-03-07 23:23:33 +0900

    Add MainVC

--- MyTube/MainVC.swift
+++ MyTube/MainVC.swift
@@ -8,18 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // TODO
+        // Logic goes here.. 
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
